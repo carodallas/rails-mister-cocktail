d14de41855cf83bdfcd704fe8446c8b402bbe4a0
carolinedallaserra <caroline.dalla.serra.26@gmail.com>
2020-08-13 22:44:28 +0200

    add exo 2

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -15,6 +15,12 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
   private
 
   def dose_params
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :cocktails do
     resources :doses, only: [:new, :create]
   end
+  resources :doses, only: [ :destroy ]
   # get 'cocktails/index', to: "cocktails#index"
   # get "cocktails/:id", to: "cocktail#show", as: :cocktail
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
