1a947b764d5fb41683e3471d76c58657d8ffbb67
carolinedallaserra <caroline.dalla.serra.26@gmail.com>
2020-08-13 17:17:23 +0200

    add exo 2

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,40 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+  private
+
+    def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  # get 'cocktails/index', to: "cocktails#index"
+  # get "cocktails/:id", to: "cocktail#show", as: :cocktail
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
