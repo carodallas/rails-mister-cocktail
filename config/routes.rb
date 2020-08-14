f80d1b203ea9c9a5ec8613b626d4c117229d87e5
carolinedallaserra <caroline.dalla.serra.26@gmail.com>
2020-08-14 14:47:52 +0200

    add route cocktail#index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [ :destroy ]
+  root to: "cocktails#index"
   # get 'cocktails/index', to: "cocktails#index"
   # get "cocktails/:id", to: "cocktail#show", as: :cocktail
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
